175ebf777e4666b790517a64313558b9be48ea4e
Qurbaneli <vqurbaneli@gmail.com>
2024-06-26 16:39:19 +0400

    responsive fixes services pages

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./services.scss";
 //Import icons
@@ -7,16 +7,18 @@ import service2 from "../../assets/icons/services/service-2.svg";
 import service3 from "../../assets/icons/services/service-3.svg";
 import service4 from "../../assets/icons/services/service-4.svg";
 import service5 from "../../assets/icons/services/service-5.svg";
-import arrow from "../../assets/icons/common/arrow.svg"
+import arrow from "../../assets/icons/common/arrow.svg";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Services() {
   useEffect(() => {
     AOS.init();
-  }, [])
+  }, []);
 
+  const [moreServices, setMoreServices] = useState(false);
+  
   const servicesData = [
     {
       id: 1,
@@ -59,32 +61,55 @@ function Services() {
       desc: "Miqrasiya hüququ",
     },
   ];
+  const maxServices= !moreServices ? 4 : servicesData.length;
   return (
     <section id="services">
       <div className="container">
-        <h2 data-aos="fade-down" className="section-title">Xidmətlərimiz</h2>
+        <h2 data-aos="fade-down" className="section-title">
+          Xidmətlərimiz
+        </h2>
         <p data-aos="fade-down" className="services-section-desc">
           Legit S.A komandası olaraq, müştərilərə aşağıdakı hüquqi xidmətlərin
           göstərilməsini təklif edə bilərik:
         </p>
         <div className="services-box">
-          {servicesData.map((item,index) => (
-            <Link data-aos="zoom-in-up"  data-aos-delay={index*100} to={`/service/${item.id}`} key={item.id}>
-            <div className="service-item">
-              <div className="service-item-overlay">
-                  <div className="service-item-overlay-icon"><img src={arrow} alt="arrow" /></div>
-              </div>
-              <div className="services-item-detail">
-                <div className="services-item-icon">
-                  <img src={item.icon} alt="service-icon" />
-                </div>
 
-                <p className="services-item-desc">{item.desc}</p>
+          {servicesData.map((item, index) => {
+            if(index<maxServices) {
+            return (
+            <Link
+              data-aos="zoom-in-up"
+              data-aos-delay={index * 100}
+              to={`/service/${item.id}`}
+              key={item.id}
+            >
+              <div className="service-item">
+                <div className="service-item-overlay">
+                  <div className="service-item-overlay-icon">
+                    <img src={arrow} alt="arrow" />
+                  </div>
+                </div>
+                <div className="services-item-detail">
+                  <div className="services-item-icon">
+                    <img src={item.icon} alt="service-icon" />
+                  </div>
+
+                  <p className="services-item-desc">{item.desc}</p>
+                </div>
               </div>
-            </div>
             </Link>
-          ))}
+          )}
+        })}
+
+
         </div>
+
+        <div
+            onClick={() => setMoreServices(!moreServices)}
+            className="btn-more"
+          >
+            {moreServices ? "Daha az" : "Daha çox"}
+          </div>
       </div>
     </section>
   );
