a6ea98dd03f809b420dcd9d080617d3d36b66fc3
Qurbaneli <vqurbaneli@gmail.com>
2024-07-02 15:29:41 +0400

    data added

--- src/pages/ServiceDetail/ServiceDetail.jsx
+++ src/pages/ServiceDetail/ServiceDetail.jsx
@@ -3,89 +3,48 @@ import "./service-detail.scss";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import { useParams } from "react-router-dom";
+import { MdKeyboardArrowRight } from "react-icons/md";
+//import Datas
+import { servicesData } from "./data";
 
 function ServiceDetail() {
-  const servicesData = [
-    {
-      id: 1,
-      desc: "Korporativ Hüquq",
-    },
-    {
-      id: 2,
-      desc: "Əmək münasibətləri",
-    },
-    {
-      id: 3,
-      desc: "Müqavilə Hüququ",
-    },
-    {
-      id: 4,
-      desc: "Sığorta Hüququ",
-    },
-    {
-      id: 5,
-      desc: "İnzibati Hüquq",
-    },
-    {
-      id: 6,
-      desc: "Əqli Mülkiyyət Hüququ",
-    },
-    {
-      id: 7,
-      desc: "Ailə Hüququ",
-    },
-    {
-      id: 8,
-      desc: "Miqrasiya hüququ",
-    },
-  ];
-  const {id}=useParams();
-  const service=servicesData.find(item=>item.id==id)
+  const { id } = useParams();
+  const service = servicesData.find((item) => item.id == id);
   return (
     <>
-     <Header />
+      <Header />
       <div className="page-header">
-       
         <h2 className="page-title">Xidmətlər</h2>
       </div>
 
       <section id="service-detail">
         <div className="container">
-          {service ? (<>
-            <h2 className="page-title">{service.desc}</h2>
-          <div className="service-detail-content">
-            <p>
-              • Yerli və xarici hüquqi şəxslərin təsis olunması, sənədlərin
-              hazırlanması və qeydiyyatı;
-            </p>
-            <p>
-              • Hüquqi şəxslərin təsis sənədlərində dəyişikliklərin, əlavələrin,
-              dövlət qeydiyyatına alınması;
-            </p>
-            <p>
-              • Hüquqi şəxslərin təsis sənədlərində dəyişikliklərin, əlavələrin,
-              dövlət qeydiyyatına alınması;
-            </p>
-            <p>• Filial, şöbə, nümayəndəliklərin təsis edilməsi;</p>
-            <p>
-              • Hüquqi şəxslərin qeydiyyatının ləğv edilməsi və ya
-              dayandırılması;
-            </p>
-            <p>
-              • Hüquqi şəxslərin fəaliyyətinin hüquqi ekspertizası (due
-              diligence);
-            </p>
-            <p>• Müxtəlif istiqamətlər üzrə müqavilələrin hazırlanması;</p>
-            <p>
-              • Hüquqi şəxslərin nizamnaməsinin, daxili qaydalarının,
-              əsasnamələrinin, starteji sənədlərinin hazırlanması;
-            </p>
-            <p>• Korporativ mübahisələrin həlli.</p>
-          </div>
-          </>) : (
+          {service ? (
+            <>
+              <h2 className="page-title">{service.title}</h2>
+              <div className="service-detail-content">
+                <p>{service.desc}</p>
+
+                <h3>
+                  Legit S.A korporativ hüquq üzrə aşağıdakı xidmətləri təklif
+                  edir:
+                </h3>
+
+                <ul>
+                  {service.services.map((item) => (
+                    <li>
+                      <MdKeyboardArrowRight />
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+
+                <p className="note">{service.note}</p>
+              </div>
+            </>
+          ) : (
             <h2 className="no-data">Məlumat tapılmadı</h2>
           )}
-  
         </div>
       </section>
 
