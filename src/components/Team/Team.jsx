9bfeb7d54f73ff318076df15ccaffdf396791d72
coderseyfi <seyfaddinmn@code.edu.az>
2025-06-13 12:26:48 +0400

    last change added script to html

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -24,8 +24,6 @@ function Team() {
 
   const teamMembers = useSelector((state) => state.team.teamMembers.data);
 
-  console.log(teamMembers);
-
   useEffect(() => {
     dispatch(getTeamAsync());
   }, [lang]);
